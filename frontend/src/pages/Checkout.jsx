6fbedc8e20cf5dbe52ee9b260ee501cb8612c3a1
tawhidtasin77 <tawhidtasin1762@gmail.com>
2026-09-04 22:20:29 +0600

    fix: correct casing of imported CSS file in Checkout component

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 import { AuthContext } from "../context/AuthContext";
-import "../app.css"
+import "../App.css"
 import { api } from "../api/api";
 
 const Checkout = () => {
